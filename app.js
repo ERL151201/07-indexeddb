f1f01910ea14df064ed44b8c747669ef4f005d66
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-10-20 18:11:20 -0500

    Inicios en indexedDB

---
+++ app.js
@@ -0,0 +1,14 @@
+// indexedDB: Reforzamiento
+let request = window.indexedDB.open('mi-database', 1);
+
+// Se actualiza cuando se crea o se sube de version de la DB
+
+request.onupgradeneeded = event => {
+    console.log('Actualizacion de BD');
+
+    let db = event.target.result;
+
+    db.createObjectStore('heroes', {
+        keyPath: 'id '
+    });
+};
